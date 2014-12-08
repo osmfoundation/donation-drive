661c75f846ee307443baba839e5eac27d017252f
tristen <tristen.brown@gmail.com>
2014-12-08 11:28:27 -0800

    WIP Drop in a rework of the site

---
+++ site.js
@@ -0,0 +1,12 @@
+
+
+$('#amount-custom').on('change', function() {
+  $('#custom-value').focus();
+});
+
+$('#donate').submit(function() {
+  console.log('foo');
+  e.preventDefault();
+  e.stopPropagation();
+  // Validate
+});
