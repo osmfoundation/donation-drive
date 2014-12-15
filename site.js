b8e8b5a70561858df07bb627a50afd55484d6ce3
tristen <tristen.brown@gmail.com>
2014-12-15 17:43:06 -0500

    draw up the form values, update bg graphic

--- site.js
+++ site.js
@@ -2,8 +2,34 @@ var goal = 100000; // Target value to be raised.
 var raised = 25000; // Value raised so far.
 var backers = 250; // Number of folks donated.
 
+// Helper function: Add commas to longer integers.
 function commaNum(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function validNumber(x) {
+  return x;
+}
+
+// Helper function: Turn form values into an object.
+
+function formKeyVal(form) {
+    var record = {};
+    ['input', 'textarea'].forEach(function(type) {
+        $(form).find(type).each(function() {
+            if (this.value.length < 1 ||
+                this.name === '') return;
+            if (this.type === 'checkbox') {
+                record[this.name] = this.checked ? true : false;
+            } else if (this.type === 'radio') {
+                if (!this.checked) return;
+                record[this.name] = this.id;
+            } else {
+                record[this.name] = this.value;
+            }
+        });
+    });
+    return record;
 }
 
 $('input[type="radio"]').on('change', function() {
@@ -17,3 +43,20 @@ $('#amount-custom').on('change', function() {
 $('#js-raised').text('£' + commaNum(raised));
 $('#js-progress').css('width', (raised / goal) * 100 + '%');
 $('#js-backers').text(backers);
+
+// Form submission
+$('#donate').submit(function(e) {
+  var array = formKeyVal(this), amount;
+  if (array.amount === 'amount-custom') {
+    amount = array['custom-value'];
+  } else {
+    amount = array.amount.split('-')[1];  
+  }
+
+  if (!validNumber) return alert('Value must be a valid number');
+
+  // Amount to be submitted to
+  // payment vendor.
+  console.log(amount);
+  return false;
+});
