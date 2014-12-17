0d71881a504fe98fe2d748480a085e7862e41030
tristen <tristen.brown@gmail.com>
2014-12-17 14:42:25 -0500

    Add textarea and limit char length to 140

--- site.js
+++ site.js
@@ -9,42 +9,64 @@ function commaNum(x) {
 
 // Helper function: Turn form values into an object.
 function formValues(form) {
-    var record = {};
-    ['input', 'textarea'].forEach(function(type) {
-        d3.select(form).selectAll(type).each(function() {
-            if (this.value.length < 1 ||
-                this.name === '') return;
-            if (this.type === 'checkbox') {
-                record[this.name] = this.checked ? true : false;
-            } else if (this.type === 'radio') {
-                if (!this.checked) return;
-                record[this.name] = this.id;
-            } else {
-                record[this.name] = this.value;
-            }
-        });
+  var record = {};
+  ['input', 'textarea'].forEach(function(type) {
+    d3.select(form).selectAll(type).each(function() {
+      if (this.value.length < 1 ||
+        this.name === '') return;
+      if (this.type === 'checkbox') {
+        record[this.name] = this.checked ? true : false;
+      } else if (this.type === 'radio') {
+        if (!this.checked) return;
+        record[this.name] = this.id;
+      } else {
+        record[this.name] = this.value;
+      }
     });
-    return record;
+  });
+  return record;
+}
+
+var count = 140;
+function changeCount() {
+  var v = this.value.length;
+  var c = d3.select('#count');
+  c.text(count - v);
 }
 
 d3.select('#js-raised').text('£' + commaNum(raised));
 d3.select('#js-progress').style('width', (raised / goal) * 100 + '%');
 d3.select('#js-backers').text(backers);
 
+d3.select('textarea')
+  .on('keydown', changeCount)
+  .on('cut', changeCount)
+  .on('paste', changeCount);
+
 // Form submission
 d3.select('#donate').on('submit', function(e) {
   d3.event.preventDefault();
   d3.event.stopPropagation();
   var array = formValues(this), amount;
-  if (array.amount === 'amount-custom') {
-    amount = array['custom-value'];
-  } else {
-    amount = array.amount.split('-')[1];  
-  }
 
-  // Amount to be submitted to
-  // payment vendor.
-  console.log(amount);
+  // Validate on messsage length
+  if (array.message.length > 140) {
+      window.alert('Limit your note to 140 characters.');
+  } else {
+    if (array.amount === 'amount-custom') {
+      amount = array['custom-value'];
+    } else {
+      amount = array.amount.split('-')[1];
+    }
+
+    var vals = {
+      amount: amount,
+      message: array.message
+    };
+
+    // Amount to be submitted to payment vendor.
+    console.log(vals);
+  }
 });
 
 // Tab selection
