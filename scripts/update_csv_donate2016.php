55633a49411115cd58915e64a0406f228f2a2e3d
Grant Slater <git@firefishy.com>
2016-11-12 00:51:35 +0000

    Add matching donations code

--- scripts/update_csv_donate2016.php
+++ scripts/update_csv_donate2016.php
@@ -16,10 +16,17 @@ $count=0;
 fputcsv($fp, array('name','amount','currency','amount_eur','message','premium')) OR die('FAILED writing header.');
 
 $randval = mt_rand(0, 1);
+
 // $launch_partners = array(array('Mapbox','It\'s a pleasure to chip in for this funding drive. Keep up the good work!'),array('Mapzen','Mapzen is happy to support the great work of the Operations Working Group!'));
 
 // fputcsv($fp, array($launch_partners[$randval][0],'20000','USD','13046.19',$launch_partners[$randval][1],'true')) OR die('FAILED writing first partner line.');
 // fputcsv($fp, array($launch_partners[1-$randval][0],'20000','USD','13046.19',$launch_partners[1-$randval][1],'true')) OR die('FAILED writing second partner line.');
+$sql_query_matching_comments = 'SELECT sum(`amount_gbp`) `amount_for_matching_gbp` FROM `donations` WHERE `processed` = 1 AND `target` = "donate2016" AND `timestamp` >= "2016-11-11 23:00:00"';
+$sql_matching_result = mysql_query($sql_query_matching_comments, $_DB_H) OR die('FAIL UPDATING: '.$sql_query_matching_comments);
+$contrib_matching = mysql_fetch_array($sql_matching_result ,MYSQL_ASSOC)  OR die('FAIL RESULT: '.$sql_query_matching_comments);
+$contrib_matching_amount_eur = $contrib_matching['amount_for_matching_gbp'] * 1.17987;
+if ($contrib_matching_amount_eur > 10000) $contrib_matching_amount_eur = 10000;
+fputcsv($fp, array('Mapbox',number_format($contrib_matching_amount_eur, 2, '.', ''),'EUR',number_format($contrib_matching_amount_eur, 2, '.', ''),'Mapbox matched donations','true')) OR die('FAILED writing first partner line.');
 
 if ($sql_result AND mysql_num_rows($sql_result)>0) {
   while($contrib = mysql_fetch_array($sql_result ,MYSQL_ASSOC)) {
