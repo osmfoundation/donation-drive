5f19390419530f1a479467b89578a1dc04813fb6
Ilya Zverev <zverik@textual.ru>
2018-01-30 14:04:55 +0300

    Monthly payments

--- process/paypal-submit.php
+++ process/paypal-submit.php
@@ -38,15 +38,23 @@ $paypal_fields = array();
 $paypal_fields['on0']			= 'contribution_tracking_id';
 $paypal_fields['os0']			= $sql_insert_id;
 $paypal_fields['business']		= 'treasurer@openstreetmap.org';
-$paypal_fields['item_name']		= 'One-time donation';
 $paypal_fields['item_number']	= 'DONATE';
 $paypal_fields['cmd']			= '_xclick';
 $paypal_fields['no_note']		= '0';
 $paypal_fields['notify_url']	= 'https://donate.openstreetmap.org/process/paypal-callback.php';
 $paypal_fields['return']		= 'https://wiki.openstreetmap.org/wiki/DonationThankYou';
 $paypal_fields['currency_code']	= (empty($data['currency']) ?  'GBP' : $data['currency']);
-$paypal_fields['amount']		= (double) $data['amount'];
 $paypal_fields['charset']		= 'utf-8';
+if (isset($_POST['monthly'])) {
+  $paypal_fields['item_name'] = 'Monthly donation';
+  $paypal_fields['a3']  = (double) $data['amount'];
+  $paypal_fields['p3']  = '1';  # 1 = every
+  $paypal_fields['t3']  = 'M';  # M = month
+  $paypal_fields['src'] = '1';  # means recurring until cancelled
+} else {
+  $paypal_fields['item_name']		= 'One-time donation';
+  $paypal_fields['amount']		= (double) $data['amount'];
+}
 $pp_url_part=array();
 foreach($paypal_fields AS $pp_key => $pp_value) {
 	$pp_url_part[] = $pp_key.'='.urlencode($pp_value);
